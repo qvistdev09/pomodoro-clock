5be321a1b0d6b61c4c55987a4500f06a32f6b415
Qvistsson <63651194+qvistsson@users.noreply.github.com>
2020-06-30 09:55:37 +0200

    Updates

---
+++ precache-manifest.a4b514cc3c347c35e67267b33e13e4ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "512e5e45c61eb1706831108e0ca1f6bd",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "6f717d87ffc262ce26d0",
+    "url": "/pomodoro-clock/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8b1d8faec31372119aa6",
+    "url": "/pomodoro-clock/static/js/2.ee259608.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-clock/static/js/2.ee259608.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f717d87ffc262ce26d0",
+    "url": "/pomodoro-clock/static/js/main.07a4b219.chunk.js"
+  },
+  {
+    "revision": "d420e891230cef74bfe7",
+    "url": "/pomodoro-clock/static/js/runtime-main.cd73fa2f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pomodoro-clock/static/media/logo.5d5d9eef.svg"
+  }
+]);
